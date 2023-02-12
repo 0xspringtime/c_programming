43d8914496ce62832700ad4c4a7575b99989bfb8
0xspringtime <0xspringtime@protonmail.com>
2023-02-12 09:15:13 -0500

    commit

---
+++ chapter_1/06/copy.s
@@ -0,0 +1,34 @@
+	.file	"copy.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	call	getchar@PLT
+	movl	%eax, -4(%rbp)
+	jmp	.L2
+.L3:
+	movl	-4(%rbp), %eax
+	movl	%eax, %edi
+	call	putchar@PLT
+	call	getchar@PLT
+	movl	%eax, -4(%rbp)
+.L2:
+	cmpl	$-1, -4(%rbp)
+	jne	.L3
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 12.2.0"
+	.section	.note.GNU-stack,"",@progbits

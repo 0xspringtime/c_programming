700bd77350229761f10b8503399da662b10a372c
0xspringtime <0xspringtime@protonmail.com>
2023-02-13 13:31:18 -0500

    commit

---
+++ chapter_1/07/print_eof.s
@@ -0,0 +1,30 @@
+	.file	"print_eof.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"EOF: %d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$-1, %esi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 12.2.0"
+	.section	.note.GNU-stack,"",@progbits

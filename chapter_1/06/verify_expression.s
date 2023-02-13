737afcf083931da7fa730f9cfcaa1e86724dc2d3
0xspringtime <0xspringtime@protonmail.com>
2023-02-13 13:24:42 -0500

    commit

---
+++ chapter_1/06/verify_expression.s
@@ -0,0 +1,34 @@
+	.file	"verify_expression.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"value of expression: %d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	call	getchar@PLT
+	cmpl	$-1, %eax
+	setne	%al
+	movzbl	%al, %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 12.2.0"
+	.section	.note.GNU-stack,"",@progbits
